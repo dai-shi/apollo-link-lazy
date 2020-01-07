a0ece6ec528431b8c69e0a8367f868bac917823e
daishi <daishi@axlight.com>
2020-01-08 08:46:12 +0900

    update microbundle@next

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,2 +1,2 @@
-import{ApolloLink as n,fromPromise as t,toPromise as r,Observable as e}from"apollo-link";var o=function(o){return new n((function(u,f){return t(o().then((function(t){return r((t instanceof n?t:t.default).request(u,f)||e.of())})))}))};export{o as lazy};
+import{ApolloLink as n,fromPromise as t,toPromise as r,Observable as e}from"apollo-link";var o=function(o){return new n(function(u,f){return t(o().then(function(t){return r((t instanceof n?t:t.default).request(u,f)||e.of())}))})};export{o as lazy};
 //# sourceMappingURL=index.esm.js.map
